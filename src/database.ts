ae075f42796f944f7a7f7574ef1a3a97d6671d7e
Duy Nguyễn <nguyendangduy269@gmail.com>
2024-12-06 16:49:13 +0700

    format code

--- src/database.ts
+++ src/database.ts
@@ -1,7 +1,7 @@
-import { Collection, Db , MongoClient } from 'mongodb';
-import { config } from 'dotenv';
-import { User } from './models/user';
-import { Key } from './models/key';
+import { Collection, Db, MongoClient } from "mongodb";
+import { config } from "dotenv";
+import { User } from "./models/user";
+import { Key } from "./models/key";
 
 config();
 
@@ -13,28 +13,27 @@ let db: Db;
 export async function connectToDatabase() {
   try {
     await client.connect();
-    console.log('Connected to MongoDB');
+    console.log("Connected to MongoDB");
     db = client.db(process.env.DATABASE_NAME);
     return db;
-
   } catch (error) {
-    console.error('Failed to connect to MongoDB', error);
+    console.error("Failed to connect to MongoDB", error);
     throw error;
   }
 }
 
 export function getUsersCollection(): Collection<User> {
-  if(!db) {
-    throw new Error('Database is not connected');
+  if (!db) {
+    throw new Error("Database is not connected");
   }
 
-  return db.collection<User>('users');
+  return db.collection<User>("users");
 }
 
 export function getKeysCollection(): Collection<Key> {
-  if(!db) {
-    throw new Error('Database is not connected');
+  if (!db) {
+    throw new Error("Database is not connected");
   }
 
-  return db.collection<Key>('keys');
+  return db.collection<Key>("keys");
 }
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,27 @@
-import express from 'express';
-import { connectToDatabase } from './database';
-import userRoutes from './routes/userRoutes';
-import keyRoutes from './routes/keyRoutes';
+import express from "express";
+import { connectToDatabase } from "./database";
+import userRoutes from "./routes/userRoutes";
+import keyRoutes from "./routes/keyRoutes";
 
 const app = express();
 const port = 3000;
-const prelix = '/api/v1';
+const prelix = "/api/v1";
 
 app.use(express.json());
 
-connectToDatabase().then((db) => {
-  app.get('/', (req, res) => {
-    res.send('Hello, world!');
-  });
+connectToDatabase()
+  .then((db) => {
+    app.get("/", (req, res) => {
+      res.send("Hello, world!");
+    });
 
-  app.use(`${prelix}/users`, userRoutes);
-  app.use(`${prelix}/keys`, keyRoutes);
+    app.use(`${prelix}/users`, userRoutes);
+    app.use(`${prelix}/keys`, keyRoutes);
 
-  app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+    app.listen(port, () => {
+      console.log(`Server is running at http://localhost:${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Failed to start server", error);
   });
-}).catch((error) => {
-  console.error('Failed to start server', error);
-});
